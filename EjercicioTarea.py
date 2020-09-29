6f7f8ffa9c77d37d825ff487cc8aacef1b4a10df
ENGELHARD14 <72060532+ENGELHARD14@users.noreply.github.com>
2020-09-29 15:29:38 -0600

    Add files via upload

---
+++ EjercicioTarea.py
@@ -0,0 +1,24 @@
+listaRegistro = []
+clientes = 0
+
+
+while clientes < 1972972:
+    cliente = input("nombre del cliente: ")
+    producto = input("producto: ")
+    costo = float(input("costo ($0.00): "))
+    detener = input("Ingresar la palabra si para detener ")
+    if detener == ("si"):
+        break
+    # punto de programa
+    # registro = {"cliente": cliente, "producto": producto, "costo": costo}
+    registro = dict(costo=costo)
+    # como agrego un elemento nuevo a una lista?
+    listaRegistro.append(registro)
+    # dejar de ocupar la variable registro
+    # registro = None
+    clientes += 1
+
+suma = sum(costo())
+
+for registro in listaRegistro:
+    print(registro)
